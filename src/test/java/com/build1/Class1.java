81580fcd98af99fc4086c039ba64b05c9b75a3e7
Mendepoojitha123 <153059453+Mendepoojitha123@users.noreply.github.com>
2023-12-19 15:06:57 +0530

    Add files via upload
    my class file is added

---
+++ src/test/java/com/build1/Class1.java
@@ -0,0 +1,11 @@
+package com.build1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hii hello i am in jenkins build ");
+
+	}
+
+}
